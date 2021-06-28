d29f8e7685d3ac62e4a075f4e6f3a33b395b08e8
Meg McGregor <megmcgregor1@gmail.com>
2021-06-28 12:11:40 -0700

    checking out to approve pull requests

---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Utils;
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository, ICommentRepository
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+
+        public List<Comment> GetAllPublishedComments()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT c.Id, 
+                               c.PostId, 
+                               c.UserProfileId, 
+                               c.Subject, 
+                               c.Content, 
+                               c.Create
+                        FROM Comment c
+                               LEFT JOIN Post p on c.PostId = p.id
+                        WHERE  c.PostId == p.Id
+                    ";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Comment> comments = new List<Comment>();
+                    while (reader.Read())
+                    {
+                        Comment comment = new Comment
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+                            UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                            Subject = reader.GetString(reader.GetOrdinal("ubject)
+                          
+
+                        dogs.Add(dog);
+                    }
+
+                    reader.Close();
+
+                    return dogs;
+                }
+            }
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Repositories
+{
+    public class ICommentRepository
+    {
+    }
+}
