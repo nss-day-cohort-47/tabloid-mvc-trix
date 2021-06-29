0653cba5f82a0be76ae73b0d9b39cc8dc1ee7ffb
Brandon Hill <brandonjon26@gmail.com>
2021-06-29 16:00:36 -0500

    i think I'm heading in the right direction

---
+++ TabloidMVC/Models/ViewModels/PostDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class PostDetailsViewModel
+    {
+        public Post Post { get; set; }
+        public List<Tag> TagOptions { get; set; }
+    }
+}
